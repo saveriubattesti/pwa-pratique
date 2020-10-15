ae98c3a2e0e503b7076347b486ea9b1d2bb201f7
Saveriu Battesti <saveriubattesti@gmail.com>
2020-10-15 09:22:34 +0200

    7. Notifications - résultat

--- main.js
+++ main.js
@@ -30,3 +30,34 @@ if(navigator.serviceWorker) {
         .register('sw.js')
         .catch(err => console.error('service worker NON enregistré', err));
 }
+
+//..
+//7.1 Notifications non persistantes
+// // Vérifie si la fonctionalité est disponible et si 
+// l'utilisateur n'a pas refusé les notifications
+/*
+if(window.Notification && window.Notification !== "denied"){
+    // demande une permission
+    Notification.requestPermission(perm => {
+        // vérifie si la permission est acceptée par l'utilisateur
+        // 3 valeurs possibles : default | granted | denied
+        if(perm === "granted"){
+            
+            // 7.2 Option de la notification
+            const options = {
+                body : "Body de la notification",
+                icon : "images/icons/icon-72x72.png"
+            }
+ 
+            // On crée une nouvelle notification
+            // 7.2 On passe les options en deuxième argument
+            const notif = new Notification("Hello notification", options);
+          
+        }
+        else{
+            // Notification refusée
+            console.log("Notification refusée");
+        }
+    })
+}
+*/
